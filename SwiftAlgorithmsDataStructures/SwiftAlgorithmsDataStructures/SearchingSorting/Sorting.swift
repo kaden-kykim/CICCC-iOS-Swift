c73d0e0ea306c15021aac640f30a278fda819239
kaden-kykim <kaden.kykim@gmail.com>
2020-02-11 00:51:30 -0800

    Add Quick sort Function

--- SwiftAlgorithmsDataStructures/SwiftAlgorithmsDataStructures/SearchingSorting/Sorting.swift
+++ SwiftAlgorithmsDataStructures/SwiftAlgorithmsDataStructures/SearchingSorting/Sorting.swift
@@ -87,5 +87,21 @@ func mergeSort<T : Comparable>(_ collection: [T], _ comparator: (T, T) -> Bool) 
 }
 
 func quickSort<T : Comparable>(_ collection: [T], _ comparator: (T, T) -> Bool) -> [T] {
-    return []
+    if collection.count <= 1 {
+        return collection
+    }
+    
+    let pivotIndex = Int.random(in: 0..<collection.count)
+    let pivotValue = collection[pivotIndex]
+    var lArr = [T](), rArr = [T]()
+    for (index, element) in collection.enumerated() where index != pivotIndex {
+        comparator(element, pivotValue) ? lArr.append(element) : rArr.append(element)
+    }
+    
+    var sortedCollection = [T]()
+    sortedCollection.append(contentsOf: quickSort(lArr, comparator))
+    sortedCollection.append(pivotValue)
+    sortedCollection.append(contentsOf: quickSort(rArr, comparator))
+    
+    return sortedCollection
 }
