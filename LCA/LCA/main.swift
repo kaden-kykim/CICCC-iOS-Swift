2fdfe2f07f8449d9f813e0f7a6ffde2959f25358
kaden-kykim <kaden.kykim@gmail.com>
2020-02-28 00:21:42 -0800

    Change Test case script logic to reference

--- LCA/LCA/main.swift
+++ LCA/LCA/main.swift
@@ -14,37 +14,11 @@ for result in lcaResults {
     print(result)
 }
 
-/// For Test case files
-//let dataPath = "/Users/Kyeongyul/Workspace/CICCC/Mobile/Dataset/LCA/TestCases/"
-//let inExt = ".in", outExt = ".out"
-//let fileManager = FileManager()
-//var testCases = [String]()
-//try! testCases = fileManager.contentsOfDirectory(atPath: dataPath)
-//for content in testCases {
-//    let dotIndex = content.firstIndex(of: ".")
-//    if let dotIndex = dotIndex {
-//        if dotIndex == content.startIndex {
-//            continue
-//        }
-//        let fileName = content.prefix(upTo: dotIndex)
-//        let ext = content.suffix(from: dotIndex)
-//        if !fileManager.fileExists(atPath: "\(dataPath)\(fileName)\(outExt)") {
-//            print("There is no output file")
-//            continue
-//        }
-//        if ext == inExt {
-//            freopen("\(dataPath)\(fileName)\(ext)", "r", stdin)
-//            let start = DispatchTime.now()
-//            let lcaTree = LCATree()
-//            let lcaResults = lcaTree.getLCA()
-//            let end = DispatchTime.now()
-//            freopen("\(dataPath)\(fileName)\(outExt)", "r", stdin)
-//            var gt = [Int]()
-//            while let line = readLine() {
-//                gt.append(Int(line)!)
-//            }
-//            print("LCA Case \(fileName): \((gt == lcaResults) ? "Correct" : "Incorrect"), " +
-//                "Time: \(Double(end.uptimeNanoseconds - start.uptimeNanoseconds) / 1_000_000)ms")
-//        }
-//    }
+
+// Below: For Testcase Files
+//func invokeFunc() -> [Int] {
+//    return LCATree().getLCA()
 //}
+//
+//let testCasePath = "/Users/Kyeongyul/Workspace/CICCC/Mobile/Dataset/LCA/TestCases/"
+//testCaseScript(testCasePath: testCasePath, invokeFunction: invokeFunc)
