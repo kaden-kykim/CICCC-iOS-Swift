27b2817b32291a544d0320a822c7d6cf2269df5a
kaden-kykim <kaden.kykim@gmail.com>
2020-02-28 23:28:41 -0800

    Change DFS logic: Recursive function to Using a stack BFS -> DFS (do not use a queue data structure)

--- SushiReviews/SushiReviews/SushiReviews/SushiReviews.swift
+++ SushiReviews/SushiReviews/SushiReviews/SushiReviews.swift
@@ -8,8 +8,6 @@
 
 import Foundation
 
-let bfsQueue = Queue<Int>()
-
 func sushiReviews() -> Int {
     let numOfSets = readLine()!.split(separator: " ")
     let numOfRestaurants = Int(numOfSets[0])!
@@ -18,9 +16,7 @@ func sushiReviews() -> Int {
     var adjacents = [[Int]](repeating: [Int](), count: numOfRestaurants)
     var checkRealSushi = [Bool](repeating: false, count: numOfRestaurants)
     
-    for real in realSushiList {
-        checkRealSushi[Int(real)!] = true
-    }
+    for real in realSushiList { checkRealSushi[Int(real)!] = true }
     
     for _ in 1..<numOfRestaurants {
         let vertices = readLine()!.split(separator: " ")
@@ -29,92 +25,93 @@ func sushiReviews() -> Int {
         adjacents[v].append(u)
     }
     
-    var startNode = 0, maxDist = 0
-    (startNode, _) = getDistancesWithMaxIndex(from: startNode, checkRealSushi: &checkRealSushi, adjacents: adjacents)
-    (startNode, _) = getDistancesWithMaxIndex(from: startNode, checkRealSushi: &checkRealSushi, adjacents: adjacents)
-    (_, maxDist) = getDistancesWithMaxIndex(from: startNode, checkRealSushi: &checkRealSushi, adjacents: adjacents)
-    print("\(startNode) \(maxDist)")
-    
-    var totalTime = 0
-    var visited = [Bool](repeating: false, count: numOfRestaurants)
-    _ = dfsForRealSushi(curNode: startNode, totalTime: &totalTime, &visited, checkRealSushi, adjacents)
-    totalTime -= maxDist
-    
-    print("Total Time: \(totalTime)")
-    return totalTime
+    let (startNode, _) = getMaxDistanceWithIndex(from: 0, checkRealSushi: checkRealSushi, adjacents: adjacents)
+    let (_, maxDist) = getMaxDistanceWithIndex(from: startNode, checkRealSushi: checkRealSushi, adjacents: adjacents)
+
+    return dfsForRealSushi(rootNode: startNode, checkRealSushi, adjacents) - maxDist
 }
 
-fileprivate func dfsForRealSushi(curNode: Int, totalTime: inout Int, _ visited: inout [Bool],
-                                 _ checkRealSushi: [Bool], _ adjacents: [[Int]]) -> Bool {
-    visited[curNode] = true
-    var hasRealSushi = checkRealSushi[curNode]
+fileprivate func dfsForRealSushi(rootNode: Int, _ checkRealSushi: [Bool], _ adjacents: [[Int]]) -> Int {
+    let dfsStack = Stack<Int>()
+    let numOfNodes = adjacents.count
     
-    for v in adjacents[curNode] {
-        if !visited[v] {
-            totalTime += 1
-            let hasChildRealSushi = dfsForRealSushi(curNode: v, totalTime: &totalTime, &visited, checkRealSushi, adjacents)
-            hasRealSushi = hasRealSushi || hasChildRealSushi
-            if hasChildRealSushi {
-                totalTime += 1
-            } else {
-                totalTime -= 1
+    var visited = [Bool](repeating: false, count: numOfNodes)
+    var hasChildRealSushi = [Bool](repeating: false, count: numOfNodes)
+    
+    dfsStack.push(item: rootNode)
+
+    while !dfsStack.isEmpty() {
+        let pop = dfsStack.pop()!
+        let hasRealSushi = checkRealSushi[pop]
+        
+        if !visited[pop] {
+            hasChildRealSushi[pop] = hasRealSushi
+            for v in adjacents[pop] {
+                if !visited[v] {
+                    dfsStack.push(item: pop)
+                    dfsStack.push(item: v)
+                    if checkRealSushi[v] { hasChildRealSushi[pop] = true }
+                }
+            }
+            visited[pop] = true
+        } else {
+            if hasChildRealSushi[pop] {
+                let peek = dfsStack.peek()
+                if let peek = peek {
+                    if visited[peek] && !hasChildRealSushi[peek] { hasChildRealSushi[peek] = true }
+                }
             }
         }
     }
     
-    return hasRealSushi
+    var total = 0
+    for b in hasChildRealSushi { if b { total += 1 } }
+    
+    return (total - 1) * 2
 }
 
-fileprivate func getDistancesWithMaxIndex(from root: Int, checkRealSushi: inout [Bool], adjacents: [[Int]]) -> (Int, Int) {
+fileprivate func getMaxDistanceWithIndex(from root: Int, checkRealSushi: [Bool], adjacents: [[Int]]) -> (Int, Int) {
+    let dfsStack = Stack<Int>()
     let numOfNodes = adjacents.count
     var visited = [Bool](repeating: false, count: numOfNodes)
-    var distances = [Int](repeating: 0, count: numOfNodes)
-    clearBFSQueue()
-    bfsQueue.enqueue(item: root)
-    while !bfsQueue.isEmpty() {
-        let curNode = bfsQueue.dequeue()!
-        visited[curNode] = true
-        for adj in adjacents[curNode] {
-            if !visited[adj] {
-                distances[adj] = distances[curNode] + 1
-                bfsQueue.enqueue(item: adj)
+    var depth = [Int](repeating: 0, count: numOfNodes)
+    
+    var maxIndex = root, maxValue = depth[root]
+    
+    dfsStack.push(item: root)
+    while !dfsStack.isEmpty() {
+        let pop = dfsStack.pop()!
+        for v in adjacents[pop] {
+            if !visited[v] {
+                dfsStack.push(item: v)
+                depth[v] = depth[pop] + 1
+                if checkRealSushi[v] && maxValue < depth[v] {
+                    maxIndex = v
+                    maxValue = depth[v]
+                }
             }
         }
+        visited[pop] = true
     }
     
-    var maxDist = 0, maxIndex = root
-    for i in 0..<numOfNodes {
-        if checkRealSushi[i] && (maxDist < distances[i]) {
-            maxDist = distances[i]
-            maxIndex = i
-        }
-    }
-    return (maxIndex, maxDist)
-}
-
-fileprivate func clearBFSQueue() {
-    while !bfsQueue.isEmpty() {
-        _ = bfsQueue.dequeue()
-    }
+    return (maxIndex, maxValue)
 }
 
 //
-//  Queue.swift
+//  Stack.swift
 //  SwiftAlgorithmsDataStructures
 //
 //  Created by Derrick Park on 2/12/20.
 //  Copyright © 2020 Derrick Park. All rights reserved.
 //
 
-/// The Queue class represents a first-in-first-out (FIFO) queue of generic items.
-/// It supports the usual *eunque* and *dequeue* operations, along with methods for peeking at the first item, testing if the queue is empty, and iterating through the items in FIFO order.
+/// The Stack class represents a last-in-first-out (LIFO) stack of generic items.
+/// It supports the usual *push* and *pop* operations, along with methods for peeking at the top item, testing if the stack is empty, and iterating through the items in LIFO order.
 /// This implementation uses a singly linked list with an inner class for linked list nodes.
-public final class Queue<E> : Sequence {
-    /// beginning of queue
+public final class Stack<E> : Sequence {
+    /// top of stack
     private var first: Node<E>? = nil
-    /// end of queue
-    private var last: Node<E>? = nil
-    /// size of the queue
+    /// size of the stack
     private(set) var count: Int = 0
     
     /// helper linked list node class
@@ -127,39 +124,36 @@ public final class Queue<E> : Sequence {
         }
     }
     
-    /// Initializes an empty queue.
+    /// Initializes an empty stack.
     public init() {}
     
-    /// Returns true if this queue is empty.
-    public func isEmpty() -> Bool {
-        return count == 0
-    }
-    
-    /// Returns the item least recently added to this queue.
-    public func peek() -> E? {
-        return last?.item ?? nil
-    }
-    
-    /// Adds the item to this queue
+    /// Adds the item to this stack
     /// - Parameter item: the item to add
-    public func enqueue(item: E) {
-        let newNode = Node.init(item: item)
-        if let last = last { last.next = newNode }
-        last = newNode
-        if isEmpty() { first = self.last }
+    public func push(item: E) {
+        first = Node(item: item, next: first)
         count += 1
     }
     
-    /// Removes and returns the item on this queue that was least recently added.
-    public func dequeue() -> E? {
+    /// Removes and returns the item most recently added to this stack.
+    public func pop() -> E? {
         guard let first = first else { return nil }
         self.first = first.next
         count -= 1
         return first.item
     }
     
-    /// QueueIterator that iterates over the items in FIFO order.
-    public struct QueueIterator<E> : IteratorProtocol {
+    /// Returns (but does not remove) the item most recently added to this stack.
+    public func peek() -> E? {
+        return first?.item ?? nil
+    }
+    
+    /// Returns true if this stack is empty.
+    public func isEmpty() -> Bool {
+        return count == 0
+    }
+    
+    /// StackIterator that iterates over the items in LIFO order.
+    public struct StackIterator<E> : IteratorProtocol {
         private var current: Node<E>?
         
         fileprivate init(_ first: Node<E>?) {
@@ -176,13 +170,13 @@ public final class Queue<E> : Sequence {
         public typealias Element = E
     }
     
-    /// Returns an iterator that iterates over the items in this Queue in FIFO order.
-    public __consuming func makeIterator() -> QueueIterator<E> {
-        return QueueIterator<E>(first)
+    /// Returns an iterator that iterates over the items in this Stack in LIFO order.
+    public __consuming func makeIterator() -> StackIterator<E> {
+        return StackIterator<E>(first)
     }
 }
 
-extension Queue: CustomStringConvertible {
+extension Stack: CustomStringConvertible {
     public var description: String {
         return self.reduce(into: "") { $0 += "\($1) " }
     }
