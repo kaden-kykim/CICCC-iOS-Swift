4f5757b1af25e7aea2e416182aa26451bc89b165
kaden-kykim <kaden.kykim@gmail.com>
2020-03-13 14:17:50 -0700

    Add DP Assignment: 10. Sum of Square numbers 2

---
+++ DPAssignments/DPAssignments/SumOfSquareNumbers/SumOfSquareNumbers2.swift
@@ -0,0 +1,44 @@
+//
+//  SumOfSquareNumber2.swift
+//  DPAssignments
+//
+//  Created by Kaden Kim on 2020-03-13.
+//  Copyright © 2020 CICCC. All rights reserved.
+//
+
+import Foundation
+
+func sumOfSquareNumbers2(_ N: Int) -> Int {
+    var sqNums = [Int]()
+    for sqrt in 1..<N {
+        let sqNum = sqrt * sqrt
+        if sqNum < N {
+            sqNums.append(sqNum)
+        } else if sqNum == N {
+            return 1
+        } else {
+            break
+        }
+    }
+    
+    var check = [Bool](repeating: false, count: N)
+    var stage = 1
+    var subtracts = [Int]()
+    subtracts.append(N)
+    while true {
+        var curSub = [Int]()
+        for curNum in subtracts {
+            for sqNum in sqNums {
+                let subNum = curNum - sqNum
+                if subNum > 0 && !check[subNum] {
+                    check[subNum] = true
+                    curSub.append(subNum)
+                } else if subNum == 0 {
+                    return stage
+                }
+            }
+        }
+        stage += 1
+        subtracts = curSub
+    }
+}
--- DPAssignments/DPAssignments/main.swift
+++ DPAssignments/DPAssignments/main.swift
@@ -18,3 +18,10 @@ print(lengthOfLIS([10,9,2,5,3,7,101,18]))
 // 5
 //print(lengthOfLIS([2,15,3,7,8,6,18]))
 
+// 10. Sum of Square numbers - 2
+// 4
+print(sumOfSquareNumbers2(7))
+// 2
+//print(sumOfSquareNumbers2(18))
+// 2
+//print(sumOfSquareNumbers2(2624))
